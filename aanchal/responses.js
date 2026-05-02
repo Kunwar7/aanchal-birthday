a80f4f439281fc466c5f9153819cd299ffa326ef
Kunwar7 <65164805+Kunwar7@users.noreply.github.com>
2026-05-02 23:16:34 +0530

    Add files via upload

---
+++ aanchal/responses.js
@@ -0,0 +1,106 @@
+const responses = [
+  {
+    name: "Vartika",
+
+    reason: "aanchal came in my life when i quite lit gave up on friendship. shes the most kind, forgiving and loving person ik plus she makes bomb ass brownies. shes been there cheering for me even without me asking her. shes so hardworking and i dont know who deserves more in this world if not her. aanchu, you warm my heart. from victims of science to forever 😛👆 HAPPY 18TH MERI JAAN",
+
+    memory: "on her 16th birthday we were on this jhula thingy and she kind of fell on me and we laughed like idiots for 5 minutes straight idk why. ALSOOOO the first time i met her, just 10 hours into meeting her and annoyed me so that i didnt sleep. kept me up till 5am. i knew then she aint leaving.",
+
+    song: "so easy by olivia dean.. shes so olivia dean coded. lover girl by laufey. LAUFEY CODED TOO. INTRO BY ARIANA GRANDE TOO. (SAARE DAALDI AHJAHAHAHA)",
+
+    photos: [
+      "vartika1.jpg",
+      "vartika2.jpg",
+      "vartika3.jpg"
+    ]
+  },
+
+  {
+    name: "aanchal's most favourite (Naiza)",
+
+    reason: "Aanchal 🫶🏽 has this way of putting a smile on every face, making every soul feel special, and being there for everybody. She is the nicest, most human being that I have come across, and that simply why she is an amazing person and the kindest one too!!",
+
+    memory: "Oh, how do I pick just one? She makes every moment feel so special. Her presence just feels like a warm hug of sunshine, but one that I wouldn’t forget would be painting with her over a pizza date at my house, watching the stupidest movie that she could have picked and having a great time because Ziven and Aanchal 🫶🏽 decided to laugh at 'Sachin aur Uncle, dono 99 Pai out'",
+
+    song: "Ishq wala lovee",
+
+    photos: [
+      "naiza1.jpg",
+      "naiza2.jpg",
+      "naiza3.jpg"
+    ]
+  },
+
+  {
+    name: "Anvi",
+
+    reason: "She's honestly the best thing that happened to me and the only reason i survived 2 years of torture at aakash",
+
+    memory: "i remember going to baskin robins with her and we were acting corny feeding each other icecream",
+
+    song: "yellow by coldplay",
+
+    photos: [
+      "anvi1.jpg",
+      "anvi2.jpg",
+      "anvi3.jpg"
+    ]
+  },
+
+  {
+    name: "kayeee",
+
+    memory: "she bakes me cookies and holds my hand tightly while crossing roads (yeah, i'd probably be dead without her)",
+
+    reason: "THERE ARE SO MANYYYYY!!!! activa pe vroom vroom, watching horror movies and hearing her say 'penchod' for the first time hahah, bullying her along abhay, and seeing her get excited over overpriced stuff. her coming to my house just cuz she's hungry and hearing her yap so much. also that one trip to dalhousie... yeah SO MANY MOMENTS, can't decide one specific fav.",
+
+    song: "last christmas",
+
+    photos: [
+      "kaye1.jpg",
+      "kaye2.jpg",
+      "kaye3.jpg"
+    ]
+  },
+
+  {
+    name: "Ami",
+
+    memory: "We have grown up together but she is the exact opposite of me when it comes to actually express love for others. I've always admired the thoughtful letters, and ofc wherever she'll go, she'll bring something special with her ;)",
+
+    reason: "late night gedi's and sneaking out without telling anyone (don't tell anyone)",
+
+    song: "Crimson and Clover (Tommy James & The Shondells)",
+
+    photos: [
+      "ami1.jpg",
+      "ami2.jpg",
+      "ami3.jpg"
+    ]
+  },
+
+  {
+    name: "Abhay",
+
+    memory: "Bc she is my sister",
+
+    reason: "Fighting",
+
+    song: "Sunflower"
+  },
+
+  {
+    name: "divjyot",
+
+    memory: "she is a reallyy caring person, always checks up on the people close to her. shes like super kindhearted and thoughtful, and has always made me feel heard. and ofcc her brownies are tasty asf. she puts thought into every little effort for the people she loves, and thats why the people she loves love her so much as well :p and also shes really fun to annoy lol. and oh she understands why ketchup is horrendous and how mint chocolate ice cream is a top flavour so bonus points fr. overall solid 11/10 person, +1 extra point for being my friend 👅😼",
+
+    reason: "well its hard to single one moment out, because shes been an amazingg friend for the 2 years ive known her, and given me a ton of memories that im fs gonna look back upon. a memorable thing is when we all went to nurpur in 11th. that was the first time we really got to know each other, and became friends. bohot cute tha to see her shenanigans there and it made me realise shes actually a really fun person hahah",
+
+    song: "Tyler the creator - Sweet / I thought you wanted to dance",
+
+    photos: [
+      "div1.jpg",
+      "div3.jpg"
+    ]
+  }
+];
